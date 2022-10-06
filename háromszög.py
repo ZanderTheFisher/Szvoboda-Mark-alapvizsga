1471a39b06e7ad404baa5693c967d8c3817ba015
Reiyji <86930801+ZanderTheFisher@users.noreply.github.com>
2022-10-06 15:49:58 +0200

    Add files via upload

---
+++ háromszög.py
@@ -0,0 +1,6 @@
+oldal = int(input("Kérem a hráomszög egyik oldalának hosszát: "))
+magassag = int(input("Kérem a háromszög magasságát: "))
+
+terulet = magassag * oldal/2
+
+print(f"A háromszög területe: {terulet}")
